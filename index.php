3c1594b344a3d3c686b4de70af404341413de092
Ian Overholt <bryceoverholt@gmail.com>
2015-09-23 11:33:02 -0500

    Update for the spiceworld app test

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>test</title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+ <head>
+  <script src="https://spiceworks.github.io/spiceworks-js-sdk/dist/spiceworks-sdk.js" type="text/javascript"></script>
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+   $(document).ready(function(){
+     var card = new SW.Card();
+     var inventory = card.services('inventory');
+     inventory.request('devices').then(function(data){
+       $.each(data.devices, function(index, device){
+         $('body').append('<p>' + device.name);
+       });
+     });
+   });
+  </script>
+ </head>
+ <body>
+  <h3>Hello world!</h3>
+ </body>
 </html>
